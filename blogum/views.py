47c2adec31cece002fa145b2cb8d16fb3163c001
JoaoVitor <joaotx17@gmail.com>
2019-05-03 21:57:00 -0300

    Agora estadinanmico

--- blogum/views.py
+++ blogum/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'blogum/post_list.html', {})
+
+    u = User.objects.get(username='admin')
+
+    posts = Post.objects.filter(author=u).order_by('published_date')
+    return render(request, 'blogum/post_list.html', {'posts': posts})
